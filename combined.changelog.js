9353038160751eb4b6e9eb56fc0a89dff5f48cef
Szalk <szalokadam@gmail.com>
2026-05-28 21:28:31 +0200

    Update combined.changelog.js

--- combined.changelog.js
+++ combined.changelog.js
@@ -1,24 +1,24 @@
 const CHANGELOG_COMBINED = {
-  "exportedAt": "2026-05-28T19:14:26.599Z",
+  "exportedAt": "2026-05-28T19:27:37.435Z",
   "apps": {
     "nstart": {
       "meta": {
         "latestVersion": "260527",
-        "exportedAt": "2026-05-28T19:14:26.602Z"
+        "exportedAt": "2026-05-28T19:27:37.439Z"
       },
       "encrypted": {
-        "iv": "i2VytNiDWnSshxWw",
-        "data": "hIRH35OGk8HJdodBJheHiFHrHcORHmI9mQ7XYwbxnncKmm6yDSeqflGzHx5QRyfvqOBPtsAhFrdINXomj14JvlRaAMJZcApaW7O+7R+NK7V2e/X4duGYoRGmO+/H0WYK2G+8j1OutpWxYsXe3usEpPxvmz6ISci+RIEP7BOiDWP5soo7kA3BPbNmk3pUkDYOWLH0Ib5g0DoZ7/F5Yer16U01oobvHvJY5IB0IhpSadGuKfjX48yx9A=="
+        "iv": "szbYHHjFNJmfcjAR",
+        "data": "wy6DCMdENO4c2Jq+tcSo7jJysjGoH5WwlIi5Iqnmq1MMuFlgLrsMOvXRQyGvMpWGH8GMxOqDOkbJrEH8R5FsuNTOsRyb+SENFoKPAG5Dz4uKGAmzuiB7bnVS6gyLV0dMqAVvX8Ya+VJM89mgzsGuucfqnAG5DLO+3pDI5pFBFB0Pa2hjBzxUqkKtG+DyW+zcJpy27hnpd85bc2mvbXnrki4v4vjG1tjdeRiIJklnGf47ACr5/azSOQ=="
       }
     },
     "maja": {
       "meta": {
         "latestVersion": "260527",
-        "exportedAt": "2026-05-28T19:14:26.603Z"
+        "exportedAt": "2026-05-28T19:27:37.441Z"
       },
       "encrypted": {
-        "iv": "fX3PZw/4dte32opB",
-        "data": "+FJ7dRAioX8kHXKgubAqqUxuZJi7kGuGQoAuDcacXHRjUhQdX2W+X4q6OQG3ZrNvwBoQ5BHbdogsliMRA9Zn8pNswcMBckOWA0jAcV2BDdQO1sDgfNrQHFzd+UZB86Ki56VhthWgDDwLiikHRk1cEPkKVb31ZetvPC7pyUaVJ1E1qWfwykSo/ElYyEX1RN5GaqBKiVCDAbqXgcfW/YylbQ=="
+        "iv": "H0OpxumT516l/bGu",
+        "data": "HzR4qswgEJFXmX2j1B32MzcWPztxR8VrOyXGZncoHNnCf26UfnCPQiCrYnz5tlr/Xv8i04leJ9wvT3Mpa7tYtQTvbHpNlBvI6y/Q5YGRZbtvPyKoHv/DaY1jps+uzk/1OWyiVDr6n+JEFy0nUJwEAQHk5JKRBEFKoyeY40K/3FlekW7EKsdFf0g/fm1vuiN2c7fb4Sy2f+4g3SQn/WkStg=="
       }
     }
   }
